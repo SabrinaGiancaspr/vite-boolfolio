84760d003d07ae5c1a8e86f99560960b8c9f2116
sabri <sabrinatwitter6@gmail.com>
2024-01-15 18:02:03 +0100

    add card component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,56 @@
+<script>
+export default{
+    props: {
+        project: {
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
+
+<template>
+<div class="card">
+    <div class="d-flex">
+        <h3 class="name">{{ project.name }}</h3>
+    
+    <p class="description"> <span class="font-strong">Description:</span> {{ project.description }}</p>
+    <p class="link"> <span class="font-strong">Link:</span> {{ project.link }}</p>
+    <p class="project-status"> <span class="font-strong">Status: </span>{{ project.project_status }}</p>
+</div>
+</div>
+
+
+</template>
+
+<style lang="scss">
+.card{
+    border: 1px solid black;
+    min-height: 400px;
+    background-color: rgba(223, 221, 221, 0.262);
+    padding: 10px;
+
+    .d-flex{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        gap: 10px;
+
+        .name{
+        border: 1px solid black;
+        margin: 15px;
+        }
+    }
+
+    .description{
+        border: 1px solid black;
+    }
+
+    .font-strong{
+        font-weight: bold
+    }
+}
+    
+    
+
+</style>
--- src/pages/projects/ProjectIndex.vue
+++ src/pages/projects/ProjectIndex.vue
@@ -1,7 +1,11 @@
 <script>
+import ProjectCard from '../../components/ProjectCard.vue';
 import axios from 'axios';
 
 export default{
+    components:{
+        ProjectCard
+    },
     data(){
         return{
             projects: []
@@ -22,10 +26,35 @@ export default{
 </script>
 
 <template>
-
+    <div class="card-container">
+        <div class="row">
+            <div class="col-4" v-for="project in projects">
+                <ProjectCard :project="project"/>
+            </div>
+        </div>
+       
+    </div>
 
 </template>
 
 <style lang="scss">
 
+.card-container{
+    max-width: 1200px;
+    margin: 0 auto;
+    margin-top: 50px;
+    padding: 20px;
+    border: 2px solid black;
+   
+    .row{
+        display: flex;
+        flex-wrap: wrap;
+        gap: 10px;
+
+        .col-4{
+            flex-basis: calc(100% * 4 / 12 - 10px);
+        }
+    }
+
+}
 </style>
