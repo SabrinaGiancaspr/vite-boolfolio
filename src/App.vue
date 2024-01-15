e5c1510c0b6b6d168179d2fe1a3d5266d649bb7a
sabri <sabrinatwitter6@gmail.com>
2024-01-15 17:18:40 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import ProjectIndex from './pages/projects/ProjectIndex.vue';
+export default{
+  components: {
+    ProjectIndex
+  }
+}
+</script>
+
+<template>
+  <div>
+    <ProjectIndex/>
+  </div>
+</template>
+
+<style lang="scss">
+@use './style/general.scss'
+
+</style>
---
+++ src/pages/projects/ProjectIndex.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+
+export default{
+    data(){
+        return{
+            projects: []
+        }
+    },
+    methods: {
+        fetchData(){
+            axios.get('http://127.0.0.1:8000/api/projects').then((res) => {
+               this.projects = res.data.results.data
+            })
+        }
+    },
+    
+    created() {
+        this.fetchData()
+    }
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style lang="scss">
+
+</style>
